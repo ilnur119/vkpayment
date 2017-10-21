c904d3d4cd4ac5a676ad89707f8a62123651ec9e
o.silakov <enegizer93@gmail.com>
2017-10-21 10:49:56 +0400

    made tinkoff api create method

--- admin/components/TinkoffAPI.php
+++ admin/components/TinkoffAPI.php
@@ -34,4 +34,34 @@ class TinkoffAPI extends Component
         }
         return null;
      }
+
+     public function createInvoice($account, $buyer_name, $inn, $bank_name, $bank_location, $bic, $corr_account, $number, $priority, $payment_day) {
+        $data = [
+            'seller' => ['account' => $account, 'bank' => []],
+            'buyer' => [
+                'name' => $buyer_name,
+                'inn' => $inn,
+                'bank' => [
+                    'name' => $bank_name,
+                    'location' => $bank_location,
+                    'bic' => $bic,
+                    'corrAccount' => $corr_account
+                ]
+            ],
+            'number' => $number,
+            'priority' => $priority,
+            'categories' => [],
+            'payment' => ['dueDate'  => $payment_day]
+        ];
+
+        $response = $this->client
+            ->post('invoice/outgoing',['body' => json_encode($data)])
+            ->addHeaders(['Authorization' => "Bearer {$this->accessToken}"])
+            ->send();
+
+        if ($response->getIsOk()) {
+            return $response->data;
+        }
+        return null;
+     }
 }
