5121c2ffa9faac6c61d48cb28a270b48b60735af
o.silakov <enegizer93@gmail.com>
2017-10-21 15:50:04 +0400

    add import products from vk method

--- admin/components/VkAPI.php
+++ admin/components/VkAPI.php
@@ -13,13 +13,29 @@ class VkAPI extends Component
 
     private $accessToken;
     private $client;
+    private $group_id;
 
-    public function __construct($accessToken, $inn, array $config = [])
+    public function __construct($accessToken, $group_id, array $config = [])
     {
-        $this->inn = $inn;
+        $this->group_id = $group_id;
         $this->accessToken = $accessToken;
         $this->client = new Client(['baseUrl' => self::BASE_URL]);
         parent::__construct($config);
+    }
+
+    public function importProducts() {
+        $version = 5.68;
+        $album_id = 0;
+        $count = 200;
+        $response = $this->client
+            ->get('market.get',['v'=>$version, 'access_token' => $this->accessToken, 'owner_id' => "-$this->group_id", 'album_id' => $album_id, 'count' => $count])
+            ->send();
+
+        if ($response->getIsOk()) {
+            return $response->data;
+        }
+
+        return response;
     }
 
 }
