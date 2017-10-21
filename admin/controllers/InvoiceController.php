de7a42de50a4e148faf442ad5c13686b6b01577a
Ilnur <ilnur11922@gmail.com>
2017-10-21 23:32:54 +0300

    no message

--- admin/controllers/InvoiceController.php
+++ admin/controllers/InvoiceController.php
@@ -24,7 +24,7 @@ class InvoiceController extends BaseController
                 $api = new TinkoffAPI(\Yii::$app->params['tinkoff.accessToken'], $app->bank->inn);
 
                 $response = $api->createInvoice($app->bank->account, $model->name, $model->inn, $model->name_bank, $model->address_bank,
-                    $model->bic, $model->corrInvoice, 222, 1, date_format(time() + 2592000, 'Y-m-d H:i:sP'));
+                    $model->bic, $model->corrInvoice, 222, 1, date('Y-m-d H:i:sP', time() + 2592000));
 
                 var_dump($response);
 
