4e1c8f183e50fed10dc4be6e93d35ab5d84f4d2b
Ilnur <ilnur11922@gmail.com>
2017-10-22 09:56:34 +0300

    no message

--- admin/views/customer/index.php
+++ admin/views/customer/index.php
@@ -2,9 +2,15 @@
     VK.callMethod("showAllowMessagesFromCommunityBox");
     VK.addCallback('onAllowMessagesFromCommunity', function f(e) {
         console.log(e);
+        VK.api('messages.isMessagesFromGroupAllowed', {
+            "group_id": <?= Yii::$app->user->identity->application->vk_group_id ?>,
+            "user_id": <?= Yii::$app->user->identity->vk_user_id ?>
+        }, function (data) {
+            console.log(data);
+        });
         VK.api("messages.send", {
-            "user_id": <?= Yii::$app->user->identity->vk_user_id ?>,
-            'message': 'Привет! Что тебя интересует?'
+            "user_id": "<?= Yii::$app->user->identity->vk_user_id ?>",
+            "message": 'Hello!'
         }, function (data) {
             console.log(data);
         });
