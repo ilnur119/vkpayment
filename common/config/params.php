04b45b9e68117a2816e4ecd3583746a62a76882b
Ilnur <ilnur11922@gmail.com>
2017-10-22 12:17:29 +0300

    update token

--- common/config/params.php
+++ common/config/params.php
@@ -7,5 +7,5 @@ return [
     'vk.secretKey' => 'WIyPQuwx2goq3UkEJGOl',
     'vk.serviceKey' => '982c6204982c6204982c6204b2987364719982c982c6204c1c5972d16c017cc76c0934b',
     'vk.iframeUrl' => 'https://vkhack.assistantly.net/admin/base/auth',
-    'tinkoff.accessToken' => 'ibDqSq_zd2Hlhq43rSS4c6FBSWs31JkrMqDrq851hwEob_Kh3fgjxtV0nSq0LgUIWN2JWJhtBGVwZwxNW2KPwA',
+    'tinkoff.accessToken' => 'oRoP-GJdjLJL_CwcRfRYh8vLI_ErREck9Y16pXVKd9sg6XSfNr69Z8COFwCaNmfwUSw_umzucuD4dn4QT54_Xw',
 ];
